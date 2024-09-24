dfbca179456265698d214bed6447e23ee7abd30f
GullibleSandwich <h.katara@iitg.ac.in>
2024-09-24 11:29:21 +0530

    Changed Terminal text

--- openhands/core/cli.py
+++ openhands/core/cli.py
@@ -43,7 +43,7 @@ def display_command(command: str):
 def display_command_output(output: str):
     lines = output.split('\n')
     for line in lines:
-        if line.startswith('[Python Interpreter') or line.startswith('openhands@'):
+        if line.startswith('[Python Interpreter') or line.startswith('curioAI@'):
             # TODO: clean this up once we clean up terminal output
             continue
         print(colored(line, 'blue'))
